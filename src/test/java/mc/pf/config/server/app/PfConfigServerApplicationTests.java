db5377c97292fed7a33afa4eea8eca53925597bd
MCrisantEV <56377073+MCrisantEV@users.noreply.github.com>
2019-11-08 15:56:26 -0500

    Update PfConfigServerApplicationTests.java

--- src/test/java/mc/pf/config/server/app/PfConfigServerApplicationTests.java
+++ src/test/java/mc/pf/config/server/app/PfConfigServerApplicationTests.java
@@ -3,7 +3,7 @@ package mc.pf.config.server.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PfConfigServerApplicationTests {
 
 	@Test
